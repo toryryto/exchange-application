294b90abc6da69a520f03cf4693fc02fb6f363f4
한송담 <hansongdam@hansongdam-ui-MacBookPro.local>
2026-02-02 23:21:48 +0900

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { App } from "@/app/App";
-import "@/styles/global.css";
+import { StrictMode } from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import { Router } from '@/router';
+import '@/styles/global.css';
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<BrowserRouter>
-				<App />
+				<Router />
 			</BrowserRouter>
 		</QueryClientProvider>
 	</StrictMode>,
