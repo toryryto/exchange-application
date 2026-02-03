ed16f0cca302892eaaf5a49ee672cfee9b084bcf
한송담 <hansongdam@hansongdam-ui-MacBookPro.local>
2026-02-04 00:18:53 +0900

    feat : add protected route

---
+++ src/features/auth/components/protected-route.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from 'react';
+
+import { Navigate } from 'react-router-dom';
+
+import { useAuth } from '../hooks/useAuth';
+
+export function ProtectedRoute({ children }: PropsWithChildren) {
+	const { isAuthenticated } = useAuth();
+
+	if (!isAuthenticated) {
+		return <Navigate to='/login' replace />;
+	}
+
+	return <>{children}</>;
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
 
+import { ProtectedRoute } from './features/auth/components/protected-route';
 import { LoginPage } from './pages/auth/login-page';
+import { HomePage } from './pages/home/home-page';
 
 export function Router() {
 	return (
 		<Routes>
 			<Route path='/login' element={<LoginPage />} />
+
+			<Route
+				path='/'
+				element={
+					<ProtectedRoute>
+						<HomePage />
+					</ProtectedRoute>
+				}
+			/>
 		</Routes>
 	);
 }
